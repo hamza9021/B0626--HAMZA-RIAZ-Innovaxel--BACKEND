fc0b75715286194db812f01c0b8185b7d8effbfe
Hamza Riaz <hriaz9803@gmail.com>
2026-06-05 15:30:38 -0700

    Add Event model with methods to read and create events from JSON file

---
+++ BACKEND/src/Models/event.model.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+class Event {
+    constructor() {
+        this.eventsFilePath = path.join(
+            __dirname,
+            "..",
+            "..",
+            "data",
+            "events.json"
+        );
+    }
+
+    readEvents() {
+        const events = JSON.parse(
+            fs.readFileSync(this.eventsFilePath, "utf-8")
+        );
+        return events;
+    }
+
+    createEvent(eventData) {
+        const events = this.readEvents();
+        events.push(eventData);
+        fs.writeFileSync(this.eventsFilePath, JSON.stringify(events));
+    }
+}

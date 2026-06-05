bfd3d9a121556b4c48828fed1bd7ec239a660f62
Hamza Riaz <hriaz9803@gmail.com>
2026-06-05 15:03:26 -0700

    Implement DB class with initialization method for data directory and JSON files

--- BACKEND/src/db/db.js
+++ BACKEND/src/db/db.js
@@ -1,1 +1,36 @@
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+class DB {
+
+    init() {
+        const dataFilePath = path.join(__dirname, "..", "..", "data");
+
+        if (!fs.existsSync(dataFilePath)) {
+            fs.mkdirSync(dataFilePath, { recursive: true });
+        }
+
+        const registrationFilePath = path.join(
+            dataFilePath,
+            "registration.json"
+        );
+
+        if (!fs.existsSync(registrationFilePath)) {
+            fs.writeFileSync(registrationFilePath, JSON.stringify([]));
+        }
+
+        const eventsFilePath = path.join(dataFilePath, "events.json");
+
+        if (!fs.existsSync(eventsFilePath)) {
+            fs.writeFileSync(eventsFilePath, JSON.stringify([]));
+        }
+
+    }
+}
+
+
+export { DB };
